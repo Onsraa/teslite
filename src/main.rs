c40c6757021e0262d610fe95becddeaed4f1ed4c
Onsraa <truongteddy306@icloud.com>
2024-12-16 15:12:20 +0100

    set up window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
-use bevy::prelude::*;
+use bevy::{
+    core::FrameCount,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowMode},
+};
+
+const WINDOW_WIDTH: f32 = 500.0;
+const WINDOW_HEIGHT: f32 = 500.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "I am a window!".into(),
+                    name: Some("bevy.app".into()),
+                    resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                    present_mode: PresentMode::AutoNoVsync,
+                    mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+                    enabled_buttons: bevy::window::EnabledButtons {
+                        maximize: false,
+                        ..Default::default()
+                    },
+                    visible: false,
+                    ..default()
+                }),
+                ..default()
+            }),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
+        .add_systems(Update, (make_visible, exit_game))
         .run();
+}
+
+fn make_visible(mut window: Single<&mut Window>, frames: Res<FrameCount>) {
+    if frames.0 == 3 {
+        window.visible = true;
+    }
+}
+
+fn exit_game (
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_events.send(AppExit::Success);
+    }
 }
